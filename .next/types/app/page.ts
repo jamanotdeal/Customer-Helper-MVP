f2680bc7d9e3a7ca85ee6036eaf29f87d8a66780
Developer-Nasim <ajnasim72@gmail.com>
2026-08-31 15:04:10 +0600

    up current changes

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Volumes/My own files/Others and business/jamanot/pwa-app/src/app/page.tsx
+// File: /Volumes/My own files/Others and business/jamanot/Customer-Helper-MVP/src/app/page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
